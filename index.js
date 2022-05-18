afae95f6142f61c522b9f3c1196356e81a1bdf79
¨Venturin¨ <diegoventurinn@gmail.com>
2022-05-18 18:38:16 -0300

    feat: requirement 02 done

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const fs = require("fs");
-const { get } = require("express/lib/response");
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = "3000";
+const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar.
-app.get("/", (_request, response) => {
+app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-
-const getTalker = () => {
-  return JSON.parse(fs.readFileSync("./talker.json", "utf8"));
-};
+const getTalker = () => JSON.parse(fs.readFileSync('./talker.json', 'utf8'));
 
 // requisito 01:
-app.get("/talker", (_req, res) => {
+app.get('/talker', (_req, res) => {
   const talkersData = getTalker();
   res.status(200).json(talkersData);
 });
 
+// requisito 02:
+app.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+  const talkersData = getTalker();
+  const talkerFound = talkersData.find((talker) => talker.id === Number(id));
+
+  if (!talkerFound) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  res.status(200).json(talkerFound);
+});
+
 app.listen(PORT, () => {
-  console.log("Online");
+  console.log('Online');
 });
