f34b628db5931f1f18e485920561d53bcb1409a1
¨Venturin¨ <diegoventurinn@gmail.com>
2022-05-18 18:25:21 -0300

    feat: requirement 01 done

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const fs = require("fs");
+const { get } = require("express/lib/response");
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = '3000';
+const PORT = "3000";
 
 // não remova esse endpoint, e para o avaliador funcionar.
-app.get('/', (_request, response) => {
+app.get("/", (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+
+const getTalker = () => {
+  return JSON.parse(fs.readFileSync("./talker.json", "utf8"));
+};
+
+// requisito 01:
+app.get("/talker", (_req, res) => {
+  const talkersData = getTalker();
+  res.status(200).json(talkersData);
+});
+
 app.listen(PORT, () => {
-  console.log('Online');
+  console.log("Online");
 });
