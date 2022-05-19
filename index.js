99f0fcb5de0473191b4b9443a041b9b656fa1ba1
¨Venturin¨ <diegoventurinn@gmail.com>
2022-05-19 20:22:29 -0300

    refactor: moved routers in separated folder

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const rescue = require('express-rescue');
 
-const crypto = require('crypto');
-const talkersInfo = require('./helpers/talker');
-
-const validatePassword = require('./middlewares/validatePassword');
-const validateEmail = require('./middlewares/validateEmail');
-const validateToken = require('./middlewares/validateToken');
-const validateNewTalker = require('./middlewares/validateNewTalker');
+const routers = require('./routers/routers');
 
 const app = express();
 app.use(bodyParser.json());
@@ -21,118 +14,7 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-// requisito 01:
-app.get('/talker', rescue(async (_req, res) => {
-  const talkersData = await talkersInfo.getTalker();
-  res.status(200).json(talkersData);
-}));
-
-// Requisito 08:
-app.get('/talker/search', validateToken, rescue(async (req, res) => {
-  const { q } = req.query;
-  const talkersData = await talkersInfo.getTalker();
-
-  if (q === undefined || q === '') {
-    res.status(200).json(talkersData);
-  }
-
-  const talkerDataFiltered = talkersData.filter((talker) =>
-    talker.name.includes(q));
-
-  res.status(200).json(talkerDataFiltered);
-}));
-
-// requisito 02:
-app.get('/talker/:id', rescue(async (req, res) => {
-  const { id } = req.params;
-  const talkersData = await talkersInfo.getTalker();
-  const talkerFound = talkersData.find((talker) => talker.id === Number(id));
-
-  if (!talkerFound) {
-    return res
-      .status(404)
-      .json({ message: 'Pessoa palestrante não encontrada' });
-  }
-
-  res.status(200).json(talkerFound);
-}));
-
-// requisito 03 e 04:
-function generateToken() {
-  return crypto.randomBytes(8).toString('hex');
-}
-
-app.post(
-  '/login',
-  validatePassword.validatePasswordExist,
-  validatePassword.validatePasswordFormat,
-  validateEmail.validateEmailExist,
-  validateEmail.validateEmailFormat,
-  rescue((_req, res) => {
-    const token = generateToken();
-
-    res.status(200).json({ token });
-  }),
-);
-
-// requisito 05:
-app.post(
-  '/talker',
-  validateToken,
-  validateNewTalker.validateNewTalkerName,
-  validateNewTalker.validateNewTalkerAge,
-  validateNewTalker.validateNewTalkerTalk,
-  validateNewTalker.validateNewTalkerDate,
-  validateNewTalker.validateNewTalkerRate,
-  rescue(async (req, res) => {
-    const newTalker = req.body;
-    const talkersData = await talkersInfo.getTalker();
-    newTalker.id = talkersData.length + 1;
-    const newTalkersData = [...talkersData, newTalker];
-    talkersInfo.addTalker(newTalkersData);
-
-    res.status(201).json(newTalker);
-  }),
-);
-
-// Requisito 06:
-app.put(
-  '/talker/:id',
-  validateToken,
-  validateNewTalker.validateNewTalkerName,
-  validateNewTalker.validateNewTalkerAge,
-  validateNewTalker.validateNewTalkerTalk,
-  validateNewTalker.validateNewTalkerDate,
-  validateNewTalker.validateNewTalkerRate,
-  rescue(async (req, res) => {
-    const { id } = req.params;
-    const editedTalker = req.body;
-    const talkersData = await talkersInfo.getTalker();
-
-    const talkerDataFiltered = talkersData.filter(
-      (talker) => talker.id !== Number(id),
-    );
-
-    editedTalker.id = Number(id);
-    const editedTalkersData = [...talkerDataFiltered, editedTalker];
-    talkersInfo.addTalker(editedTalkersData);
-
-    res.status(200).json(editedTalker);
-  }),
-);
-
-// Requisito 07:
-app.delete('/talker/:id', validateToken, rescue(async (req, res) => {
-  const { id } = req.params;
-  const talkersData = await talkersInfo.getTalker();
-
-  const talkerDataFiltered = talkersData.filter(
-    (talker) => talker.id !== Number(id),
-  );
-  talkersInfo.addTalker(talkerDataFiltered);
-
-  res.status(204).json();
-}));
+app.use('/', routers);
 
 app.use((err, req, res, _next) => {
   res.status(500).json({ error: `Erro: ${err.message}` });
